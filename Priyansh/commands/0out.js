0a8aebe0feeac8625d34d6ad93ee215127848c68
Saurabh Thakur <165843437+SAURABHXWD7@users.noreply.github.com>
2025-06-25 09:41:19 +0530

    Update 0out.js

--- Priyansh/commands/0out.js
+++ Priyansh/commands/0out.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: "out",
     version: "1.0.0",
     hasPermssion: 2,
-    credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+    credits: "Saurabh Thakur",
     description: "",
     commandCategory: "Admin",
     usages: "out [id]",
