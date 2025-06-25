d3f900e8fc4e5ea8e25d1a3097348694d3640030
Saurabh Thakur <165843437+SAURABHXWD7@users.noreply.github.com>
2025-06-25 09:41:43 +0530

    Update 0shayri.js

--- Priyansh/commands/0shayri.js
+++ Priyansh/commands/0shayri.js
@@ -3,7 +3,7 @@ module.exports.config = {
   name: "shayri",
   version: "1.0.1",
   hasPermssion: 0,
-  credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+  credits: "Saurabh Thakur",
   description: "Ranmdom a Shayri",
   commandCategory: "group",
   usages: "shayri",
